780c633a508e97e1a08e91a31e37ac2ad6e831b9
artytim <arty.tim@gmail.com>
2017-03-29 14:50:08 +0800

    Display Text Values - Display strings with TextView

--- app/src/main/java/com/artytect/android/artysdesignui/MainActivity.java
+++ app/src/main/java/com/artytect/android/artysdesignui/MainActivity.java
@@ -2,6 +2,7 @@ package com.artytect.android.artysdesignui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,9 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView productText = (TextView) findViewById(R.id.product_name);
-        String productName =
-                String.format(getString(R.string.delicious_pie), "Apple");
-        productText.setText(productName);
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setMovementMethod(new ScrollingMovementMethod());
     }
 }
