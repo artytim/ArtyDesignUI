64e63e838c31c3a5891d99cf7d4dbaf3e0664703
artytim <arty.tim@gmail.com>
2017-03-30 10:42:18 +0800

    Display Text Values - Manage text entry with EditText

--- app/src/main/java/com/artytect/android/artysdesignui/MainActivity.java
+++ app/src/main/java/com/artytect/android/artysdesignui/MainActivity.java
@@ -2,17 +2,27 @@ package com.artytect.android.artysdesignui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    EditText editName, editPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editName = (EditText) findViewById(R.id.editName);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+    }
 
-        TextView tv = (TextView) findViewById(R.id.textView);
-        tv.setMovementMethod(new ScrollingMovementMethod());
+    public void clickHandler(View view) {
+        String name = editName.getText().toString();
+        String password = editPassword.getText().toString();
+
+        Log.i(TAG, "clickHandler: name=" + name + ", password=" + password);
     }
 }
